6b7813f3c1f446286d7e00f89024c625a928313d
Seccond <133043584+Seccond@users.noreply.github.com>
2026-01-14 21:59:01 +0900

    왼쪽 사이드 헤더 기본 레이아웃 구성

---
+++ src/assets/icons/index.js
@@ -0,0 +1,26 @@
+export { default as homeIcon } from "./home.svg";
+export { default as homeActiveIcon } from "./home-solid.svg";
+
+export { default as searchIcon } from "./search.svg";
+export { default as searchActiveIcon } from "./search-solid.svg";
+
+export { default as exploreIcon } from "./explore.svg";
+export { default as exploreActiveIcon } from "./explore-solid.svg";
+
+export { default as reelsIcon } from "./reels.svg";
+export { default as reelsActiveIcon } from "./reels-solid.svg";
+
+export { default as messagesIcon } from "./messages.svg";
+export { default as messagesActiveIcon } from "./messages-solid.svg";
+
+export { default as notificationsIcon } from "./notifications.svg";
+export { default as notificationsActiveIcon } from "./notifications-solid.svg";
+
+export { default as createIcon } from "./create.svg";
+export { default as createActiveIcon } from "./create-solid.svg";
+/*
+export { default as profileIcon } from "./profile.svg";
+export { default as profileActiveIcon } from "./profile-solid.svg";
+*/
+export { default as menuIcon } from "./menu.svg";
+export { default as appsIcon } from "./apps.svg";
